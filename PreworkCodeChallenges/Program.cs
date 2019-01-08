92eea23fdf43edd5c5b14aaba2967005bfe4ff12
andrewquamme <andrewquamme@gmail.com>
2019-01-07 19:02:36 -0800

    completes problem 1

--- PreworkCodeChallenges/Program.cs
+++ PreworkCodeChallenges/Program.cs
@@ -6,7 +6,32 @@ namespace PreworkCodeChallenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ProblemOne();
+            ProblemTwo();
+        }
+
+        static void ProblemOne()
+        {
+            Console.WriteLine("Enter five numbers between 1 & 10, separated by spaces");
+            string userInput = Console.ReadLine();
+            string[] numbers = userInput.Split();
+            Console.WriteLine($"Enter a number from the following list: {string.Join(", ", numbers)}");
+            string numberSelection = Console.ReadLine();
+
+            int count = 0;
+            foreach(string num in numbers){
+                if(num == numberSelection){
+                    count++;
+                }
+            }
+            Console.WriteLine($"The number {numberSelection} has a score of {int.Parse(numberSelection) * count}");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+        }
+
+        static void ProblemTwo()
+        {
+
         }
     }
 }
